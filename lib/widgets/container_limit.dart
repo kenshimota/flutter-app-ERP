b84eb74c0e6d455ef7b35e249351a75f46871f59
jessica <morenoyesica605@gmail.com>
2023-09-19 11:48:53 -0400

    cree un archivo, y le agrege la clase container limit

---
+++ lib/widgets/container_limit.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ContainerLimit extends StatelessWidget {
+  final double maxWidth;
+  final double maxHeight;
+  final double minWidth;
+  final double minHeight;
+  final Color? color;
+  final Widget? child;
+
+  const ContainerLimit({
+    super.key,
+    this.maxWidth = double.infinity,
+    this.maxHeight = double.infinity,
+    this.minWidth = 0,
+    this.minHeight = 0,
+    this.child,
+    this.color = Colors.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    double screenWidth =
+        maxWidth == double.infinity || maxWidth <= width ? maxWidth : width;
+    double screenHeight = maxHeight == double.infinity || maxHeight <= height
+        ? maxHeight
+        : height;
+
+    return LimitedBox(
+      maxWidth: screenWidth,
+      maxHeight: screenHeight,
+      child: OverflowBox(
+        maxWidth: screenWidth,
+        maxHeight: screenHeight,
+        minHeight: minHeight,
+        minWidth: minWidth,
+        child: FractionallySizedBox(
+          widthFactor: 1,
+          child: Container(
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
