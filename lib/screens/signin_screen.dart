132d3ad7ebfb62beef7120f59f571756730a81e5
jessica <morenoyesica605@gmail.com>
2023-09-19 11:56:05 -0400

    borre unas importaciones, que ya no necesitaba, en este archivo

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_erp/widgets/form_signin.dart';
 import 'package:flutter_app_erp/widgets/layourt_twice_builder.dart';
 import 'package:logger/logger.dart';
 import 'package:flutter_app_erp/widgets/signin_screen_desktop.dart';
-import 'package:flutter_app_erp/widgets/container_limit.dart';
+import 'package:flutter_app_erp/widgets/signin_screen_mobile.dart';
 
 class SigninScreen extends StatelessWidget {
   const SigninScreen({super.key});
@@ -18,28 +17,6 @@ class SigninScreen extends StatelessWidget {
     return const LayourtTwiceBuilder(
       mobile: SigninScreenMobile(),
       desktop: SigninScreenDesktop(),
-    );
-  }
-}
-
-class SigninScreenMobile extends StatelessWidget {
-  const SigninScreenMobile({super.key});
-
-  onPressed() {
-    Logger logger = Logger();
-    logger.w("test: fluytter");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: ContainerLimit(
-        maxWidth: 500,
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: FormSignin(),
-        ),
-      ),
     );
   }
 }
