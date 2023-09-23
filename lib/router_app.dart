5f3e6cd6abab7425678ca5706fd49da9e962f19b
jessica <morenoyesica605@gmail.com>
2023-09-22 22:49:45 -0400

    se modificaron las rutas

--- lib/router_app.dart
+++ lib/router_app.dart
@@ -5,7 +5,7 @@ import 'package:flutter_app_erp/screens/signup_screen.dart';
 import 'package:flutter_app_erp/screens/home_screen.dart';
 
 final GoRouter router = GoRouter(routes: [
-  GoRoute(path: "/home", builder: (context, state) => const HomeScreen()),
-  GoRoute(path: "/", builder: (context, state) => const SigninScreen()),
+  GoRoute(path: "/", builder: (context, state) => const HomeScreen()),
+  GoRoute(path: "/signin", builder: (context, state) => const SigninScreen()),
   GoRoute(path: "/signup", builder: (context, state) => const SignupScreen())
 ]);
