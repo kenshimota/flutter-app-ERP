1977b6e3b7987f15ec34286341120f500a33e343
jessica <morenoyesica605@gmail.com>
2023-09-22 22:50:12 -0400

    se creo un archivo autho provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthProvider extends ChangeNotifier {
+  Future<void> signIn(String email, String password) async {
+    // Implement sign-in logic here
+  }
+
+  Future<void> signUp(String email, String password) async {
+    // Implement sign-up logic here
+  }
+
+  Future<void> signOut() async {
+    // Implement sign-out logic here
+  }
+}
