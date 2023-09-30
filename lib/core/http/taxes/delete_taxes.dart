54e8f36f8303676b7eb8b623cf9673519d49db28
yesica <morenoyesica605@gmail.com>
2023-09-30 12:29:01 -0400

    cree un archivo delete

---
+++ lib/core/http/taxes/delete_taxes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+
+Future<bool> deleteTax({ required int id, required String token  }) async {
+
+   final env = dotenv.env;
+  final String hostname = env['HOSTNAME_API'] ?? '';
+  final Uri url = Uri.parse("$hostname/taxes/$id");
+
+  final Map<String, String> headers = {
+    'content-Type': 'application/json',
+    "Authorization": "Bearer ${token}",
+  };
+
+  http.Response response = await http.delete(url, headers: headers);
+
+
+  return 204 == response.statusCode;
+}
